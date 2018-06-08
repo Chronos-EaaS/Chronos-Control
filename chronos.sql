5093f19ec8c3fa2ac599af6d36dd60fcf321fae8
Sein Coray <s3inlc@hashes.org>
2018-06-08 10:31:59 +0200

    updated initial sql file

--- chronos.sql
+++ chronos.sql
@@ -1,13 +1,18 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- version 4.7.8
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 CREATE TABLE `Evaluation` (
   `evaluationId` int(11) NOT NULL,
@@ -15,9 +20,9 @@ CREATE TABLE `Evaluation` (
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   `systemId` int(11) NOT NULL,
   `experimentId` int(11) NOT NULL,
-  `internalId` int(11) NOT NULL
+  `internalId` int(11) NOT NULL,
+  `isArchived` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 
 
 CREATE TABLE `Event` (
@@ -31,29 +36,26 @@ CREATE TABLE `Event` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-
 CREATE TABLE `Experiment` (
   `experimentId` int(11) NOT NULL,
   `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `userId` int(11) NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
-  `type` int(11) NOT NULL,
   `systemId` int(11) NOT NULL,
   `phases` int(11) NOT NULL,
   `status` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `projectId` int(11) NOT NULL,
   `postData` text COLLATE utf8_unicode_ci NOT NULL,
-  `internalId` int(11) NOT NULL
+  `internalId` int(11) NOT NULL,
+  `isArchived` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 
 
 CREATE TABLE `Job` (
   `jobId` int(11) NOT NULL,
   `userId` int(11) NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
-  `type` int(11) NOT NULL,
   `systemId` int(11) NOT NULL,
   `environment` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `phases` int(11) NOT NULL,
@@ -69,7 +71,6 @@ CREATE TABLE `Job` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-
 CREATE TABLE `Project` (
   `projectId` int(11) NOT NULL,
   `name` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
@@ -77,16 +78,22 @@ CREATE TABLE `Project` (
   `userId` int(11) NOT NULL,
   `systemId` int(11) NOT NULL,
   `isFinished` tinyint(4) NOT NULL,
-  `environment` varchar(256) COLLATE utf8_unicode_ci NOT NULL
+  `environment` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `isArchived` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+
+CREATE TABLE `ProjectUser` (
+  `projectUserId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `projectId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 CREATE TABLE `Result` (
   `resultId` int(11) NOT NULL,
   `data` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 
 
 CREATE TABLE `Session` (
@@ -99,7 +106,6 @@ CREATE TABLE `Session` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-
 CREATE TABLE `Setting` (
   `settingId` int(11) NOT NULL,
   `section` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
@@ -107,7 +113,6 @@ CREATE TABLE `Setting` (
   `value` blob NOT NULL,
   `systemId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 
 
 CREATE TABLE `System` (
@@ -121,10 +126,8 @@ CREATE TABLE `System` (
   `vcsUser` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `vcsPassword` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime NOT NULL,
-  `lastEdit` datetime NOT NULL,
-  `builderJson` TEXT NOT NULL
+  `lastEdit` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
 
 
 CREATE TABLE `User` (
@@ -162,6 +165,10 @@ ALTER TABLE `Job`
 
 ALTER TABLE `Project`
   ADD PRIMARY KEY (`projectId`);
+
+
+ALTER TABLE `ProjectUser`
+  ADD PRIMARY KEY (`projectUserId`);
 
 
 ALTER TABLE `Result`
@@ -204,6 +211,10 @@ ALTER TABLE `Project`
   MODIFY `projectId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 
+ALTER TABLE `ProjectUser`
+  MODIFY `projectUserId` int(11) NOT NULL AUTO_INCREMENT;
+
+
 ALTER TABLE `Result`
   MODIFY `resultId` int(11) NOT NULL AUTO_INCREMENT;
 
@@ -221,7 +232,9 @@ ALTER TABLE `System`
 
 
 ALTER TABLE `User`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
