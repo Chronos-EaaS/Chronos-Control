5419f58918e1bc05d4ec99995b15300d1bc7fb46
Sein Coray <s3inlc@hashes.org>
2018-06-08 14:41:32 +0200

    fixed path

--- views/admin/systems.php
+++ views/admin/systems.php
@@ -101,12 +101,9 @@ $this->includeInlineJS("
             <div class="col-md-3">
                 <div class="box box-primary">
                     <div class="box-body box-profile">
-                        <img class="profile-user-img img-responsive" src="images/plus.png" alt="Create new System">
-
+                        <img class="profile-user-img img-responsive" src="/images/plus.png" alt="Create new System">
                         <h3 class="profile-username text-center">&nbsp;</h3>
-
                         <p class="text-muted text-center">&nbsp;</p>
-
                         <ul class="list-group list-group-unbordered">
                             <li class="list-group-item">
                                 <b>&nbsp;</b> <span class="pull-right">&nbsp;</span>
