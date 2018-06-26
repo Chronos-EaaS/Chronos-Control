8557185b4037e9158fa26e546a9f7efd0e8bcc8e
Sein Coray <s3inlc@hashes.org>
2018-06-26 10:07:48 +0200

    removed debugging

--- api/v1/job.php
+++ api/v1/job.php
@@ -247,7 +247,6 @@ class Job_API extends API {
         }
 
         $filename = realpath(UPLOADED_DATA_PATH) . "/" . $job->getId() . ".zip";
-        throw new Exception("Filename: $filename");
         if (!move_uploaded_file($_FILES[$fileUploadName]['tmp_name'], $filename)) {
             throw new Exception('Failed to move uploaded file to destination!');
         }
