94a545568f1f08615cbb3a1754141f6644eb3e0f
Marco Vogt <marco.vogt@unibas.ch>
2019-04-09 21:38:34 +0200

    Fixed builder library

--- libraries/builder.php
+++ libraries/builder.php
@@ -108,8 +108,8 @@ class Builder_Library {
                 $template = new Template("experiment/dependentGroup");
                 $obj = array('title' => $group['title'], 'id' => $group['id'], 'depends' => $group['depends'], 'dependsValue' => $group['dependsValue'], 'content' => $c);
                 $content .= $template->render($obj);
-                $js .= "$(\"name=[[depends]]\").change(function(){
-                    if ($(\"[name='[[depends]]']\").val() == \"[[dependsValue]]\") {
+                $js .= "$(\"name=" . $group['depends'] . "\").change(function(){
+                    if ($(\"[name='" . $group['depends'] . "']\").val() == \"" . $group['dependsValue'] . "\") {
                         $(\"#[[id]]\").prop('disabled',true);
                         $(\"#[[id]]\").children().prop('disabled',true);
                     } else {
@@ -118,7 +118,7 @@ class Builder_Library {
                     }
                 });
                 $( document ).ready(function() {
-                    if ($(\"[name='[[depends]]']\").val() == \"[[dependsValue]]\") {
+                    if ($(\"[name='" . $group['depends'] . "']\").val() == \"" . $group['dependsValue'] . "\") {
                         $(\"#[[id]]\").prop('disabled',true);
                         $(\"#[[id]]\").children().prop('disabled',true);
                     } else {
