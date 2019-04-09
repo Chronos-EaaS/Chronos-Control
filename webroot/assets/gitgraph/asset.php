07fa48b0dc409b838e753086f4c674f8151a31c3
Marco Vogt <marco.vogt@unibas.ch>
2019-04-09 13:11:52 +0200

    Fixed typo in gitgraph asset

--- webroot/assets/gitgraph/asset.php
+++ webroot/assets/gitgraph/asset.php
@@ -4,4 +4,4 @@ $path = explode('/' , __DIR__);
 $dirname = array_pop($path);
 
 // JS
-$this->includeJS('/assets/' . $dirname . '/gitgraph.min.css');
+$this->includeJS('/assets/' . $dirname . '/gitgraph.min.js');
