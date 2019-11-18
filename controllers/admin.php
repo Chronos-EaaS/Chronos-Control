52cffe24d600ac4624295d6ddb10859ccd9dec9e
Sein Coray <s.coray@unibas.ch>
2019-11-18 14:18:24 +0100

    test

--- controllers/admin.php
+++ controllers/admin.php
@@ -82,7 +82,7 @@ class Admin_Controller extends Controller {
                 }
             }
             file_put_contents(SERVER_ROOT . "/config.php", implode("\n", $config));
-            $this->view->redirect("/admin/update");
+            //$this->view->redirect("/admin/update");
             echo file_get_contents(SERVER_ROOT . "/config.php");
             $repository_branch = $branch;
         }
