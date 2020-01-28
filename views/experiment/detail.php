a30e56ee3e71ae6fe3d2cc0ab9aeaa96a548606e
Sein Coray <s.coray@unibas.ch>
2020-01-28 10:29:24 +0100

    testing dismissal removal

--- views/experiment/detail.php
+++ views/experiment/detail.php
@@ -98,7 +98,7 @@ $this->includeInlineJS("
                         <form role="form" action="#" method="post">
                             <div class="box-body">
                                 <div id="saveResultBox" style="display:none;" class="alert alert-success alert-dismissible">
-                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                    <button type="button" class="close" aria-hidden="true">×</button>
                                     <span id="saveResult"><h4><i class="icon fa fa-check"></i> Success</h4></span>
                                 </div>
                                 <div class="form-group">
