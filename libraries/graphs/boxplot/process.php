6dee5fe90daf548cbe54da0055e737a29343b557
Sein Coray <s.coray@unibas.ch>
2020-02-07 10:27:34 +0100

    calculating values for box plot

--- libraries/graphs/boxplot/process.php
+++ libraries/graphs/boxplot/process.php
@@ -12,6 +12,7 @@ use DBA\Job;
 /** @var $jobs Job[][] */
 /** @var $parameter string|string[] */
 /** @var $plotData string */
+/** @var $allData array */ //these are all the values which are posted by this plot
 
 $dataArray = ['datasets' => []];
 $colors = ['#00c0ef', '#3c8dbc', '#f56954'];
@@ -19,57 +20,57 @@ $colors = ['#00c0ef', '#3c8dbc', '#f56954'];
 $parameterData = [];
 $labels = [];
 
+if (!is_array($parameter)) {
+    $parameter = explode(",", $parameter);
+}
+
+// put together jobs
+$arr = Util::mergeJobs($jobs, $parameter);
+$jobGroups = $arr[0];
+$internLabels = $arr[1];
+
 $colorIndex = 0;
-foreach($jobs as $group) {
-    $results = [];
-    foreach($group as $job){
-        $results[] = json_decode($job->getResult(), true);
-    }
-    if (is_array($parameter)) {
-        foreach ($parameter as $p) {
-            if (isset($results[0][$p])) {
-                $sum = 0;
-                foreach($results as $r){
-                    $sum += $r[$p];
-                }
-                if (isset($parameterData[$p])) {
-                    $parameterData[$p]['data'][] = floatval($sum/sizeof($group));
-                } else {
-                    $parameterData[$p] = [];
-                    $parameterData[$p]['data'] = [floatval($sum/sizeof($group))];
-                    $parameterData[$p]['label'] = $p;
-                    $parameterData[$p]['fillColor'] = $colors[$colorIndex];
-                    $parameterData[$p]['strokeColor'] = $colors[$colorIndex];
-                    $parameterData[$p]['pointColor'] = $colors[$colorIndex];
-                    $parameterData[$p]['pointStrokeColor'] = $colors[$colorIndex];
-                    $parameterData[$p]['pointHighlightFill'] = '#fff';
-                    $parameterData[$p]['pointHighlightStroke'] = $colors[$colorIndex];
-                    $colorIndex++;
+for ($i = 0; $i < sizeof($jobGroups[0]); $i++) {
+    $parameterData[$i]['label'] = $internLabels[$i];
+    $parameterData[$i]['backgroundColor'] = Results_Library::colorToRGBA($colors[$colorIndex % sizeof($colors)]);
+    $parameterData[$i]['borderColor'] = $colors[$colorIndex % sizeof($colors)];
+    $parameterData[$i]['borderWidth'] = 1;
+    $colorIndex++;
+}
+
+$arr = Util::getDifferentParameters($jobs);
+$changingParameters = $arr[0];
+$jobParameters = $arr[1];
+
+foreach ($jobGroups as $jobGroup) {
+    /** @var $jobGroup Job[][] */
+    $jobIds = [];
+    $index = 0;
+    foreach ($jobGroup as $j) {
+        $arr = [];
+        foreach ($j as $job) {
+            $results = json_decode($job->getResult(), true);
+            foreach ($parameter as $p) {
+                if (isset($results[$allData['plotting']])) {
+                    $arr[] = floatval($results[$allData['plotting']]);
                 }
             }
         }
-        $labels[] = "Job " . $group[0]->getInternalId();
-    } else if (isset($results[0][$parameter])) {
-        $sum = 0;
-        foreach($results as $r){
-            $sum += $r[$parameter];
-        }
-        if (isset($parameterData[$parameter])) {
-            $parameterData[$parameter]['data'][] = floatval($sum/sizeof($group));
-        } else {
-            $parameterData[$parameter] = [];
-            $parameterData[$parameter]['data'] = [floatval($sum/sizeof($group))];
-            $parameterData[$parameter]['label'] = "Job " . $job->getInternalId();
-            $parameterData[$parameter]['fillColor'] = $colors[$colorIndex];
-            $parameterData[$parameter]['strokeColor'] = $colors[$colorIndex];
-            $parameterData[$parameter]['pointColor'] = $colors[$colorIndex];
-            $parameterData[$parameter]['pointStrokeColor'] = $colors[$colorIndex];
-            $parameterData[$parameter]['pointHighlightFill'] = '#fff';
-            $parameterData[$parameter]['pointHighlightStroke'] = $colors[$colorIndex];
-            $colorIndex++;
-        }
-        $labels[] = "Job " . $group[0]->getInternalId();
+        $parameterData[$index]['data'][] = Results_Library::boxPlotCalculation($arr);
+        $jobIds[] = $j[0]->getInternalId();
+        $index++;
     }
+    $label = [];
+    foreach ($changingParameters as $changingParameter) {
+        if (in_array($changingParameter, $parameter)) {
+            continue;
+        } else if ($changingParameter == 'run') {
+            continue;
+        }
+        $label[] = $changingParameter . ": " . $jobParameters[$jobGroup[0][0]->getId()][$changingParameter];
+    }
+
+    $labels[] = "Jobs[" . implode(", ", $label) . "]";
 }
 
 foreach ($parameterData as $pData) {
