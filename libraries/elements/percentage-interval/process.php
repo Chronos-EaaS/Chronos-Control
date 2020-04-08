2ebf9fe3127810bd1030058af08ce5d63c4157f5
Sein Coray <s3inlc@hashes.org>
2020-04-08 14:02:17 +0200

    Fix number of jobs generated from percentage-intervals from same group (#106)

--- libraries/elements/percentage-interval/process.php
+++ libraries/elements/percentage-interval/process.php
@@ -18,25 +18,41 @@ if (intval($data[$parameter . "-step"]) == 0 && intval($data[$parameter . "-star
     throw new Exception("Invalid step size causing endless loop!");
 }
 
-if ($data[$parameter . "-start"] > $data[$parameter . "-end"]) {
-    $start = $data[$parameter . "-end"];
-    $end = $data[$parameter . "-start"];
-    $step = -$data[$parameter . "-step"];
-} else {
-    $start = $data[$parameter . "-start"];
-    $end = $data[$parameter . "-end"];
-    $step = $data[$parameter . "-step"];
-}
-
-if (intval($start) == intval($end)) {
-    $step = 1;
-}
-
-for ($i = intval($start); $i <= intval($end); $i += intval($step)) {
+$count = 1;
+if (isset($allConfigurations[0][$data[$parameter . "-dependency"] . "-step"])) {
     foreach ($allConfigurations as $configuration) {
+        $s = $configuration[$data[$parameter . "-dependency"] . "-step"];
+        $value = $data[$parameter . "-start"] + $data[$parameter . "-step"] * ($s - 1);
+
         $copy = $configuration;
-        $copy[Define::CONFIGURATION_PARAMETERS][$parameter] = $i;
+        $copy[Define::CONFIGURATION_PARAMETERS][$parameter] = $value;
         $newConfigurations[] = $copy;
+    }
+} else {
+    if ($data[$parameter . "-start"] > $data[$parameter . "-end"]) {
+        $start = $data[$parameter . "-end"];
+        $end = $data[$parameter . "-start"];
+        $step = -$data[$parameter . "-step"];
+    } else {
+        $start = $data[$parameter . "-start"];
+        $end = $data[$parameter . "-end"];
+        $step = $data[$parameter . "-step"];
+    }
+
+    if (intval($start) == intval($end)) {
+        $step = 1;
+    }
+    for ($i = intval($start); $i <= intval($end); $i += intval($step), $count++) {
+        foreach ($allConfigurations as $configuration) {
+            $copy = $configuration;
+            $copy[Define::CONFIGURATION_PARAMETERS][$parameter] = $i;
+            if (intval($start) != intval($end)) {
+                // if the current setting is the special case that there will be no range due to start == end,
+                // we don't have steps as this might mess up if other ranges in the same dependency should do steps
+                $copy[$data[$parameter . "-dependency"] . "-step"] = $count;
+            }
+            $newConfigurations[] = $copy;
+        }
     }
 }
 $allConfigurations = $newConfigurations;
