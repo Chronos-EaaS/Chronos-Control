7ed6d2b3547c979df733f682aa66dd7d2437dd43
Sein Coray <s3inlc@hashes.org>
2020-08-31 15:33:23 +0200

    changed values to save for warmup default

--- views/admin/system.php
+++ views/admin/system.php
@@ -386,8 +386,8 @@ $this->includeInlineCSS("
                             <div class="form-group">
                                 <label>Warm-up Phase</label>
                                 <select id="default_phases_warmUp" name="default_phases_warmUp" class="form-control required">
-                                    <option <?php if(!isset($data['defaultValues']['phases_warmUp']) || $data['defaultValues']['phases_warmUp'] == 0) echo 'selected'; ?> value="0">unchecked</option>
-                                    <option <?php if(isset($data['defaultValues']['phases_warmUp']) && $data['defaultValues']['phases_warmUp'] == 1) echo 'selected'; ?> value="1">checked</option>
+                                    <option <?php if(!isset($data['defaultValues']['phases_warmUp']) || $data['defaultValues']['phases_warmUp'] == 0) echo 'selected'; ?> value="unchecked">unchecked</option>
+                                    <option <?php if(isset($data['defaultValues']['phases_warmUp']) && $data['defaultValues']['phases_warmUp'] == 1) echo 'selected'; ?> value="checked">checked</option>
                                 </select>
                             </div>
                         </div>
