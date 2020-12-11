b395f87df594d9658669845ef40a34a4fd4cc40a
Sein Coray <s3inlc@hashes.org>
2020-12-11 11:40:36 +0100

    Adding min-width for columns of experiments and evaluations to avoid wrapping (#154)

--- views/experiment/detail.php
+++ views/experiment/detail.php
@@ -246,7 +246,7 @@ $this->includeInlineJS("
                                     <th style="width: 10px;">#</th>
                                     <th>Name</th>
                                     <th>Description</th>
-                                    <th>&nbsp;</th>
+                                    <th style="min-width: 80px;">&nbsp;</th>
                                 </tr>
                                 </thead>
                                 <tbody>
