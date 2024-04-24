68f1ca546e5e469203caa3cb7e3b833a06ce1bbe
Marco Vogt <marco.vogt@unibas.ch>
2024-04-24 13:32:40 +0200

    Adjust dies

--- core/router.php
+++ core/router.php
@@ -134,7 +134,7 @@ if (file_exists($target)) {
         // class does not exist!
         $found = false;
         $log->debug('=> file found, but class does not exist: ' . $target);
-        die('=> class found, but action does not exist: ' . $action);
+        die('file found, but class does not exist: ' . $target);
     }
 
     // check if function exists
@@ -142,7 +142,7 @@ if (file_exists($target)) {
         // function does not exist!
         $found = false;
         $log->debug('=> class found, but action does not exist: ' . $action);
-        die('=> class found, but action does not exist: ' . $action);
+        die('class found, but action does not exist: ' . $action);
     }
     $log->debug('controller found and instantiated: ' . $class);
 } else {
