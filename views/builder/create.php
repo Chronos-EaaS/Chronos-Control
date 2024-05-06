c44b4d6c7c86b023cf4e23ef44db26f889b26806
PatrikB <p.buetler@stud.unibas.ch>
2024-05-06 16:40:57 +0200

    changed deployment choice to be required

--- views/builder/create.php
+++ views/builder/create.php
@@ -224,6 +224,8 @@ $this->includeInlineCSS("
                                             <div class="popup-content" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 20px; border-radius: 5px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5); text-align: center;">
                                                 <script>
                                                     function openPopup() {
+                                                        let depl = document.getElementById('environment').value;
+                                                        console.log(depl);
                                                         document.getElementById('popup').style.display = 'block';
                                                     }
                                                     function confirmPopup() {
@@ -234,7 +236,7 @@ $this->includeInlineCSS("
                                                         document.getElementById('popup').style.display = 'none';
                                                     }
                                                 </script>
-                                                <p>Deployment '<script>document.getElementById('environment').value </script>' was selected. Please confirm or cancel. </p>
+                                                Deployment '<script>document.getElementById('environment').value</script>' was selected. Please confirm or cancel.
                                                 <button type="button" class="popup-button primary" style="margin: 0 10px; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer; background-color: #007bff; color: white;" onclick="confirmPopup()">Confirm</button>
                                                 <button type="button" class="popup-button secondary" style="margin: 0 10px; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer; background-color: #6c757d; color: white;" onclick="closePopup()">Cancel</button>
                                             </div>
