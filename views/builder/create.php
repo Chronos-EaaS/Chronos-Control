b34f9286828a1ca7f02e6a6bf05cf07c594e5ca7
PatrikB <p.buetler@stud.unibas.ch>
2024-05-06 16:16:22 +0200

    changed deployment choice to be required

--- views/builder/create.php
+++ views/builder/create.php
@@ -220,7 +220,8 @@ $this->includeInlineCSS("
                                 <div class="row">
                                     <div class="col-xs-12">
                                         <button onclick="openPopup()" class="btn-success">Create</button>
-                                        <div id="popup" class="popup-container" style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: 9999;">                                            <div class="popup-content">
+                                        <div id="popup" class="popup-container" style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); z-index: 9999;">
+                                            <div class="popup-content" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: white; padding: 20px; border-radius: 5px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5); text-align: center;">
                                                 <p>Deployment '<script>document.getElementById('environment')</script>' was selected. Please confirm or cancel. </p>
                                                 <button type="submit" class="btn btn-block btn-success btn-lg">Confirm</button>
                                                 <button onclick="closePopup()" class="btn btn-block btn-success btn-lg">Cancel</button>
