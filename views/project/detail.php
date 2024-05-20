5aaf757cd8450be99b224b32b27f2c1e4f0a3023
PatrikB <p.buetler@stud.unibas.ch>
2024-05-20 15:46:31 +0200

    reestablished add member functionality in projects

--- views/project/detail.php
+++ views/project/detail.php
@@ -289,8 +289,8 @@ $this->includeInlineJS("
                         <div class="box-footer">
                             <form action="/project/detail/id=<?php echo $data['project']->getId() ?>" method="post" class="form-inline">
                                 <select name="member" class="form-control" title="User">
+                                    <option value="no members found">No Member found</option>
                                     <?php foreach($data['users'] as $user) { /** @var $user \DBA\User */ ?>
-                                        <?php echo $user->getUsername();?>
                                         <option value="<?php echo $user->getId(); ?>"><?php echo $user->getFirstname() . ' ' . $u->getLastname() . ' (' . $u->getUsername() . ')'; ?></option>
                                     <?php } ?>
                                 </select>
