b4d04ca384037c6e66164c5006fc6667f54d3eac
PatrikB <p.buetler@stud.unibas.ch>
2024-06-11 15:15:26 +0200

    Changed popup to be in line with project visuals and standards

--- views/builder/create.php
+++ views/builder/create.php
@@ -227,7 +227,7 @@ $this->includeInlineCSS("
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Cancel"
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"
                         onclick="document.getElementById('form').reset()">
                     <span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title">Select Deployment</h4>
