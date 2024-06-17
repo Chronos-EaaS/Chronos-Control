cde17367accb1f80efa5fc87ca65148a98d2020a
PatrikB <p.buetler@stud.unibas.ch>
2024-06-17 18:59:54 +0200

    Expermineting to drag results

--- views/results/build.php
+++ views/results/build.php
@@ -74,11 +74,9 @@ $this->includeInlineJS("
     function movePlots(direction, id) {
         if (direction == 'up') {
             alert('moving up');
-            swap('up');
         }
         elif (direction == 'down') {
             alert('moving down');
-            swap('down');
         }
         else {
             alert('direction is neither up or down');
