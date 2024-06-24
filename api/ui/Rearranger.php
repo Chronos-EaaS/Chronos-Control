e16deb33f0c27ad35ae231abfc2bf1bd3067b60f
PatrikB <p.buetler@stud.unibas.ch>
2024-06-24 16:20:46 +0200

    Expermineting to drag results

--- api/ui/Rearranger.php
+++ api/ui/Rearranger.php
@@ -58,13 +58,11 @@ class Rearranger {
         //echo "goal: " . $goal . "\n";
         //print_r($subarray, false);
         if (gettype($subarray) == 'array') {
-            foreach ($subarray as $key => $element) {
-                foreach ($element as $elementkey => $elementvalue) {
-                    echo $elementvalue . "\n";
-                    if (gettype($elementvalue) == 'string' && $elementvalue == $goal) {
-                        echo "Found " . $element . " at key: " . $key . "\n";
-                        return true;
-                    }
+            foreach ($subarray as $elementkey => $elementvalue) {
+                echo $elementvalue . "\n";
+                if (gettype($elementvalue) == 'string' && $elementvalue == $goal) {
+                    echo "Found " . $elementvalue . " at key: " . $elementkey . "\n";
+                    return true;
                 }
             }
             return false;
