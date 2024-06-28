0978cbccecee2181358e80bfb486218cc0c3fd0a
PatrikB <p.buetler@stud.unibas.ch>
2024-06-28 14:09:20 +0200

    Relocated Rearranger.php, changed include path

--- api/ui/results.php
+++ api/ui/results.php
@@ -25,7 +25,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
 
-include ("Rearranger.php");
+include "../../core/Rearranger.php";
+
+use core\Rearranger;
 
 class Results_API extends API {
 
