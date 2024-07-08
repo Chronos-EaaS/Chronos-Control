b56d9c7be7e95a3fa1f6bcaab3165e2fd9d04e2b
PatrikB <p.buetler@stud.unibas.ch>
2024-07-08 21:27:47 +0200

    implementing features

--- core/LogUtils.php
+++ core/LogUtils.php
@@ -5,7 +5,7 @@ class LogUtils {
     private $log;
     private $logLength = -1;
     private $thresholdError = 1;
-    private $thresholdWarning = 10;
+    private $thresholdWarning = 12;
     private $thresholdLogSize = 400;
     private $keyWordDict = ['ERROR:' => 0, 'WARNING:' => 0];
 
