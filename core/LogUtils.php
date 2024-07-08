8eff291d3719ba94d447cda4eefe27d99ea0e137
PatrikB <p.buetler@stud.unibas.ch>
2024-07-08 21:21:44 +0200

    implementing features

--- core/LogUtils.php
+++ core/LogUtils.php
@@ -41,7 +41,7 @@ class LogUtils {
         }
         // for now, these are fixed. potentially expand and allow any keyword
         if ($this->keyWordDict['WARNING:'] >= $this->thresholdWarning) {
-            echo "too many warnings"
+            echo "too many warnings";
             $this->job->setLogAlert('warning');
         }
         else if ($this->keyWordDict['ERROR:'] >= $this->thresholdError) {
