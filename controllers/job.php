94770e15b28866aca24c6a2bf4879bdc18974c1f
PatrikB <p.buetler@stud.unibas.ch>
2024-07-21 21:11:31 +0200

    implementing features

--- controllers/job.php
+++ controllers/job.php
@@ -151,6 +151,7 @@ class Job_Controller extends Controller {
                 $events = Util::eventFilter(['job' => $job]);
                 $this->view->assign('events', $events);
                 if (!empty($this->post['recount'])) {
+                    echo 'recount triggered';
                     $logalyzer = new Logalyzer_Library($job);
                     $logalyzer->examineEntireLog();
                 }
