1b94e06e5448fb54d85f52561f89a01a84e189b8
PatrikB <p.buetler@stud.unibas.ch>
2024-07-21 16:32:47 +0200

    fixes to finalize implementation

--- controllers/admin.php
+++ controllers/admin.php
@@ -466,7 +466,7 @@ class Admin_Controller extends Controller {
                         $logalyzer->addKey('warning', 'string', $key);
                     }
                 }
-            } else if (!empty($this->post['newErrorPattern'])) {
+            } else if (!empty($this->post['newError'])) {
                 $key = $this->post['newErrorPattern'];
                 echo $key;
                 if ($key != "") {
