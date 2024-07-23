54f47092918b3c681db02dc6b34715bb5849ba06
PatrikB <p.buetler@stud.unibas.ch>
2024-07-23 23:50:13 +0200

    debugging for stress test and processing log lines

--- api/v1/job.php
+++ api/v1/job.php
@@ -298,7 +298,9 @@ class Job_API extends API {
             mkdir(UPLOADED_DATA_PATH . 'log');
         }
         file_put_contents(UPLOADED_DATA_PATH . 'log/' . $id . '.log', $this->request['log'], FILE_APPEND);
+        echo "Creating Logalyzer.. \n";
         $logalyzer = new Logalyzer_Library($job);
         $logalyzer->examineLogLine($this->request['log']);
+        echo "Logalyzer is done.. \n";
     }
 }
