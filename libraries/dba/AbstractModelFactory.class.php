850b038be0453872fc9870d85cedbed62f65a5b7
PatrikB <p.buetler@stud.unibas.ch>
2024-07-29 20:30:41 +0200

    LogLine debugging

--- libraries/dba/AbstractModelFactory.class.php
+++ libraries/dba/AbstractModelFactory.class.php
@@ -789,18 +789,18 @@ abstract class AbstractModelFactory {
                                 @concat,
                                 CAST(CAST(JSON_UNQUOTE(
                                     JSON_EXTRACT(logalyzerResults, @concat)
-                                ) AS UNSIGNED) + :amount AS CHAR))
-                                WHERE jobId = :jobId AND JSON_SEARCH(logalyzerResults, 'one', :pattern) is not null;";
+                                ) AS UNSIGNED) + 7 AS CHAR))
+                                WHERE jobId = 29633 AND JSON_SEARCH(logalyzerResults, 'one', 'Error') is not null;";
            $stmt2 = $dbh->prepare($incrementQuery);
             if ($stmt2 === false) {
                 file_put_contents(UPLOADED_DATA_PATH . 'log/' . $jobId . '.log', "\nError in prepare()\n", FILE_APPEND);
             }
-            $stmt2->bindParam(':pattern', $pattern, PDO::PARAM_STR);
-            $stmt2->bindParam(':amount', $amount, PDO::PARAM_INT);
-            $stmt2->bindParam(':jobId', $jobId, PDO::PARAM_INT);
-            if (!$stmt2->execute()) {
-                file_put_contents(UPLOADED_DATA_PATH . 'log/' . $jobId . '.log', "\nError in execute()\n", FILE_APPEND);
-            }
+            //$stmt2->bindParam(':pattern', $pattern, PDO::PARAM_STR);
+            //$stmt2->bindParam(':amount', $amount, PDO::PARAM_INT);
+            //$stmt2->bindParam(':jobId', $jobId, PDO::PARAM_INT);
+            //if (!$stmt2->execute()) {
+            //    file_put_contents(UPLOADED_DATA_PATH . 'log/' . $jobId . '.log', "\nError in execute()\n", FILE_APPEND);
+            //}
             //$hashUpdate = "UPDATE Job SET logalyzerResults = JSON_SET(logalyzerResults, '$.hash', ?) WHERE jobId=?";
             //$stmt3 = $dbh->prepare($hashUpdate);
             //$stmt3->execute([$hash, $jobId]);
