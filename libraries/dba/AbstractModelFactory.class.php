488aa26c1d03a960e818f119631413f07166ad79
PatrikB <p.buetler@stud.unibas.ch>
2024-07-29 21:43:16 +0200

    LogLine debugging

--- libraries/dba/AbstractModelFactory.class.php
+++ libraries/dba/AbstractModelFactory.class.php
@@ -794,8 +794,10 @@ abstract class AbstractModelFactory {
             $stmt = $dbh->query("SELECT * FROM Job WHERE jobId = 29633");
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
             $stmt5 = $dbh->query("SELECT @index");
-            $res5 = $stmt5->fetch(PDO::FETCH_ASSOC);
-            file_put_contents(UPLOADED_DATA_PATH . 'log/' . $jobId . '.log', "\n".$res5."\n", FILE_APPEND);
+            $res5 = $stmt5->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($res5 as $row) {
+                file_put_contents(UPLOADED_DATA_PATH . 'log/' . $jobId . '.log', "\n".$row."\n", FILE_APPEND);
+            }
 
             foreach ($results as $row) {
                 file_put_contents(UPLOADED_DATA_PATH . 'log/' . $jobId . '.log', "\n".$row['logalyzerResults']."\n", FILE_APPEND);
