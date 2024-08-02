e092dd0b7c807f3fb8c0381773313fca0d035b57
PatrikB <p.buetler@stud.unibas.ch>
2024-08-02 21:34:50 +0200

    cleaning up files

--- api/v1/job.php
+++ api/v1/job.php
@@ -299,7 +299,10 @@ class Job_API extends API {
         }
         file_put_contents(UPLOADED_DATA_PATH . 'log/' . $id . '.log', $this->request['log'], FILE_APPEND);
         if($this->request['log'] == "clearLog") {
-            file_put_contents(UPLOADED_DATA_PATH . 'log/' . $id . '.log', "");
+            $fp = fopen("UPLOADED_DATA_PATH . 'log/' . $id . '.log'", "r+");
+            ftruncate($fp, 0);
+            fclose($fp);
+            # doesnt work file_put_contents(UPLOADED_DATA_PATH . 'log/' . $id . '.log', "");
         }
         #$logalyzer = new Logalyzer_Library($job);
         #$logalyzer->examineLogLine($this->request['log']);
