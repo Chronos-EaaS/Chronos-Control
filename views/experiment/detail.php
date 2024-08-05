80b7c8f163367eba23f1f507a34741cbe93a7c93
PatrikB <p.buetler@stud.unibas.ch>
2024-08-05 14:10:03 +0200

    Testing getResultPlots()

--- views/experiment/detail.php
+++ views/experiment/detail.php
@@ -246,9 +246,9 @@ $this->includeInlineJS("
                                     Show Archived
                                 </a>
                             <?php } ?>
-                            <a href='/results/show/id=<?php echo $data['evaluation']->getId()?>' class="btn btn-primary pull-right" title="Results">
+                            <!--<a href='/results/show/id=<?php echo $data['evaluation']->getId()?>' class="btn btn-primary pull-right" title="Results">
                                 Results
-                            </a>
+                            </a>-->
                         </div>
                         <div class="box-body">
                             <table id="evaluation" class="table table-hover">
