ae68f5ae4df5f3bc8d6da04b759a10139bbfa6ff
PatrikB <p.buetler@stud.unibas.ch>
2024-08-14 01:22:46 +0200

    fixed typo

--- libraries/dba/AbstractModelFactory.class.php
+++ libraries/dba/AbstractModelFactory.class.php
@@ -816,10 +816,10 @@ abstract class AbstractModelFactory {
                                      SET logalyzerResults = JSON_SET(
                                      logalyzerResults, 
                                      :index,
-                                     CAST(CAST(
+                                     JSON_QUOTE(CAST(CAST(
                                       JSON_UNQUOTE(
                                         JSON_EXTRACT(logalyzerResults, :index)
-                                          ) AS UNSIGNED) + :amount AS CHAR))
+                                          ) AS UNSIGNED) + :amount AS CHAR)))
                                      WHERE jobId = :jobId AND JSON_SEARCH(logalyzerResults, 'one', :pattern) is not null;";
                 $stmt2 = $dbh->prepare($incrementQuery);
                 if ($stmt2 !== false) {
