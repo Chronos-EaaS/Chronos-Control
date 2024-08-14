1097ad4a2269fb87c966f34db6c90a1f3eb5aaa5
PatrikB <p.buetler@stud.unibas.ch>
2024-08-14 02:22:54 +0200

    changed json to be 'result':[{}]

--- libraries/dba/AbstractModelFactory.class.php
+++ libraries/dba/AbstractModelFactory.class.php
@@ -814,13 +814,11 @@ abstract class AbstractModelFactory {
                 file_put_contents(UPLOADED_DATA_PATH . 'log/' . $jobId . '.log', "\nIndex is ".$index['@index']."\n", FILE_APPEND);
 
                 $checker = $dbh->prepare("UPDATE Job SET logalyzerResults = JSON_SET(
-                    logalyzerResults, '$.result[0].count', 5)
+                    logalyzerResults, '$.result[0].count', 66)
                     WHERE jobId = :jobId AND JSON_SEARCH(logalyzerResults, 'one', :pattern) is not null;");
                 $checker->bindParam(':jobId', $jobId, PDO::PARAM_INT);
                 $checker->bindParam(':pattern', $pattern['pattern'], PDO::PARAM_STR);
                 $checker->execute();
-                $fetch = $checker->fetch(PDO::FETCH_ASSOC);
-                file_put_contents(UPLOADED_DATA_PATH . 'log/' . $jobId . '.log', print_r($fetch, true)."\n", FILE_APPEND);
 
                 $checker2 = $dbh->prepare("SELECT * FROM Job WHERE jobId = :jobId AND JSON_SEARCH(logalyzerResults, 'one', :pattern) is not null;");
                 $checker2->bindParam(':jobId', $jobId, PDO::PARAM_INT);
